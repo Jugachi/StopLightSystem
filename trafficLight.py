23581d63a757b223d8c8124084500ad4624e23aa
Jugachi <contact@jugachi.com>
2025-12-02 17:10:21 +0100

    Test 2

--- trafficLight.py
+++ trafficLight.py
@@ -1,58 +1,129 @@
 from gpiozero import TrafficLights, LED, Button
 from time import sleep
 
-auto_ampel = TrafficLights(25, 8, 7)
+ampel_1 = TrafficLights(25, 8, 7)
+ampel_2 = TrafficLights(26, 19, 13)
 
 fussg_rot = LED(21)
 fussg_gruen = LED(20)
-
-# Der Knopf
 knopf = Button(16)
 
+anforderung_fussgaenger = False
 
-def ampel_zyklus():
-    print("Knopf gedrückt! Ampelphasen starten...")
 
-    sleep(2)
-    auto_ampel.green.off()
-    auto_ampel.amber.on()
-    sleep(3)
+def knopf_gedrueckt():
+    global anforderung_fussgaenger
+    anforderung_fussgaenger = True
+    print("Knopf wurde gedrückt! Anforderung gespeichert.")
 
-    auto_ampel.amber.off()
-    auto_ampel.red.on()
 
-    sleep(2)
+knopf.when_pressed = knopf_gedrueckt
 
+
+def intelligentes_warten(sekunden):
+    global anforderung_fussgaenger
+
+    schritte = int(sekunden * 10)
+
+    for _ in range(schritte):
+        if anforderung_fussgaenger:
+            return True
+        sleep(0.1)
+    return False
+
+
+def alles_auf_rot():
+    if ampel_1.green.value == 1:
+        ampel_1.green.off()
+        ampel_1.amber.on()
+        sleep(2)
+        ampel_1.amber.off()
+        ampel_1.red.on()
+
+    if ampel_2.green.value == 1:
+        ampel_2.green.off()
+        ampel_2.amber.on()
+        sleep(2)
+        ampel_2.amber.off()
+        ampel_2.red.on()
+
+    sleep(1)
+
+
+def fussgaenger_zyklus():
+    """Der Ablauf für die Fußgänger"""
+    global anforderung_fussgaenger
+    print("Fußgängerphase startet...")
+
+    alles_auf_rot()
+
+    # Fußgänger Grün
     fussg_rot.off()
     fussg_gruen.on()
-    print("Fussgänger können gehen.")
-
     sleep(5)
 
     fussg_gruen.off()
     fussg_rot.on()
-
     sleep(2)
 
-    auto_ampel.amber.on()
-    sleep(1)
-
-    auto_ampel.red.off()
-    auto_ampel.amber.off()
-    auto_ampel.green.on()
-    print("Autos haben wieder Grün.")
+    anforderung_fussgaenger = False
+    print("Fußgängerphase beendet.")
 
 
-print("Ampel-System gestartet. Drücke STRG+C zum Beenden.")
 
-auto_ampel.green.on()
+print("Kreuzungs-Steuerung aktiv.")
+print("Drücke den Knopf für Fußgänger.")
+
+ampel_1.green.on()
+ampel_2.red.on()
 fussg_rot.on()
 
 try:
     while True:
-        knopf.wait_for_press()
+        abbruch = intelligentes_warten(5)
 
-        ampel_zyklus()
+        if abbruch:
+            fussgaenger_zyklus()
+            ampel_1.red.off()
+            ampel_1.amber.on()
+            sleep(1)
+            ampel_1.amber.off()
+            ampel_1.green.on()
+            continue
+
+        ampel_1.green.off()
+        ampel_1.amber.on()
+        sleep(2)
+        ampel_1.amber.off()
+        ampel_1.red.on()
+
+        sleep(1)
+
+        ampel_2.red.off()
+        ampel_2.amber.on()  # Rot-Gelb
+        sleep(1)
+        ampel_2.amber.off()
+        ampel_2.green.on()
+
+        abbruch = intelligentes_warten(5)
+
+        if abbruch:
+            fussgaenger_zyklus()
+            continue
+
+        ampel_2.green.off()
+        ampel_2.amber.on()
+        sleep(2)
+        ampel_2.amber.off()
+        ampel_2.red.on()
+
+        sleep(1)
+
+        ampel_1.red.off()
+        ampel_1.amber.on()
+        sleep(1)
+        ampel_1.amber.off()
+        ampel_1.green.on()
 
 except KeyboardInterrupt:
-    print("\nProgramm beendet.")
+    print("Beendet")
